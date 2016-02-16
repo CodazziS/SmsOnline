541971819121c1c2fafa45d763052209efed9251
Stéphane Codazzi <stephane.codazzi@gmail.com>
2016-02-16 23:13:35 +0100

    Add message class

--- app/src/main/java/fr/codazzi/smsonline/controllers/Messages.java
+++ app/src/main/java/fr/codazzi/smsonline/controllers/Messages.java
@@ -1,4 +1,12 @@
 package fr.codazzi.smsonline.controllers;
 
+import org.json.JSONArray;
+
 public class Messages {
+
+    public JSONArray getAllMessages () {
+        JSONArray messages = new JSONArray();
+
+        return messages;
+    }
 }
