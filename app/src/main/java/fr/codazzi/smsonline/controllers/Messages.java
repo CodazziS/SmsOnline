1500f7d6fcc32ee683c4df1cca92cf3ccb1585d4
Stéphane Codazzi <stephane.codazzi@gmail.com>
2016-02-28 19:40:49 +0100

    Add resync message

--- app/src/main/java/fr/codazzi/smsonline/controllers/Messages.java
+++ app/src/main/java/fr/codazzi/smsonline/controllers/Messages.java
@@ -16,32 +16,39 @@ import fr.codazzi.smsonline.Tools;
 
 public class Messages {
 
-    private String body = null;
+    private long lastDateSms = 0;
+    private long lastDateMms = 0;
+    String unreadSmsList = "";
 
-    /*
-    public JSONArray getAllMessages () {
-        JSONArray messages = new JSONArray();
 
+    public JSONArray getLastsMessages(Context context) {
+
+        JSONArray messages = null;
+        String selection = "date > " + this.lastDateSms + " " +
+                "OR (_id IN (" + this.unreadSmsList + ") AND read = 1)";
+        JSONArray sms = this.getSms(context, selection);
+        //JSONArray mms = null;
+
+        /* TODO : Concat MMS + SMS */
+        messages = sms;
         return messages;
     }
-    */
 
     public JSONArray getAllMessages(Context context) {
+
         JSONArray messages = null;
-        Cursor sms_cursor;
+        JSONArray sms = this.getSms(context, null);
+        //JSONArray mms = null;
+
+        /* TODO : Concat MMS + SMS */
+        messages = sms;
+        return messages;
+    }
+
+    private JSONArray getMms(Context context) {
+        JSONArray mms = null;
         //Cursor mms_cursor;
         ContentResolver cr = context.getContentResolver();
-
-        /* SMS */
-        String [] projection_sms = {"address", "body", "date", "date_sent", "type", "read", "_id"};
-        //String [] projection_sms = null;
-        Uri sms_uri = Uri.parse("content://sms/");
-        sms_cursor = cr.query(sms_uri, projection_sms, null, null, null);
-        if (sms_cursor != null) {
-            messages = messagesToArray("sms", sms_cursor);
-            //logMessages("sms", sms_cursor);
-            sms_cursor.close();
-        }
 
         /* MMS */
         //String [] projection_mms = {"date", "msg_box", "read", "_id"};
@@ -53,20 +60,38 @@ public class Messages {
                 mms_cursor.close();
             }
          */
-
-        return messages;
+        return mms;
     }
 
-    public JSONArray messagesToArray(String message_type, Cursor c) {
+    private JSONArray getSms(Context context, String selection) {
+        JSONArray sms = null;
+        Cursor sms_cursor;
+        ContentResolver cr = context.getContentResolver();
+        String [] projection_sms = {"address", "body", "date", "date_sent", "type", "read", "_id"};
+        Uri sms_uri = Uri.parse("content://sms/");
+
+        sms_cursor = cr.query(sms_uri, projection_sms, selection, null, null);
+        if (sms_cursor != null) {
+            sms = messagesToArray("sms", sms_cursor);
+            sms_cursor.close();
+        }
+        return sms;
+    }
+
+    private JSONArray messagesToArray(String message_type, Cursor c) {
         JSONArray messages = new JSONArray();
         JSONObject message;
         int messagesNb;
+        String mess_id;
+
         try {
             if (c.moveToFirst()) {
+                Log.e("SMS FOUND", "NUMBER: " + c.getCount());
                 for (messagesNb = 0; messagesNb < c.getCount(); messagesNb++) {
                     if (message_type.equals("sms")) {
+                        mess_id = c.getString(c.getColumnIndex("_id"));
                         message = new JSONObject();
-                        message.put("id", c.getString(c.getColumnIndex("_id")));
+                        message.put("id", mess_id);
                         message.put("address", c.getString(c.getColumnIndex("address")));
                         message.put("date", c.getString(c.getColumnIndex("date")));
                         message.put("date_sent", c.getString(c.getColumnIndex("date_sent")));
@@ -74,6 +99,25 @@ public class Messages {
                         message.put("type", c.getString(c.getColumnIndex("type")));
                         message.put("body", c.getString(c.getColumnIndex("body")));
                         messages.put(message);
+                        if (c.getLong(c.getColumnIndex("date")) > this.lastDateSms) {
+                            this.lastDateSms = c.getLong(c.getColumnIndex("date"));
+                        }
+                        if (c.getString(c.getColumnIndex("read")).equals("0")) {
+                            if (!this.unreadSmsList.equals("")) {
+                                this.unreadSmsList += ",";
+                            }
+                            this.unreadSmsList += mess_id;
+                        } else if (this.unreadSmsList.contains(mess_id)) {
+                            /* If the ID is in the string */
+                            this.unreadSmsList = this.unreadSmsList.replace(mess_id + ",", "");
+                            /* If the ID is the first ID of the String */
+                            this.unreadSmsList = this.unreadSmsList.replace("," + mess_id, "");
+                            /* If the ID is the unique ID of the string */
+                            if (this.unreadSmsList.equals(mess_id)) {
+                                this.unreadSmsList = "";
+                            }
+                        }
+
                     } else if (message_type.equals("mms")) {
                         /* @TODO MMS */
                     }
@@ -86,6 +130,7 @@ public class Messages {
         return messages;
     }
 
+    /*
     public void logMessages (String message_type, Cursor c) {
         int messagesNb;
         int columnNb;
@@ -103,4 +148,5 @@ public class Messages {
             }
         }
     }
+    */
 }
