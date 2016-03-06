e0d3605c30f69314df53a3b42f05578ed2115c20
Stephane C <Stephane C>
2016-03-06 22:06:08 +0100

    new synchronization

--- app/src/main/java/fr/codazzi/smsonline/sync/SyncReceiver.java
+++ app/src/main/java/fr/codazzi/smsonline/sync/SyncReceiver.java
@@ -1,14 +1,25 @@
 package fr.codazzi.smsonline.sync;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
+
+import java.util.Calendar;
 
 public class SyncReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent myIntent = new Intent(context, Synchronisation.class);
-        context.startService(myIntent);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,  0, myIntent, 0);
+        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.add(Calendar.SECOND, 1); // first time
+        long frequency = 10 * 1000; // in ms
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), frequency, pendingIntent);
     }
 }
